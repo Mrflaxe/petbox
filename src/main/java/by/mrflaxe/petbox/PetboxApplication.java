33f9239d7460b2cd416fe6eb1f09e84c497746eb
Mrflaxe <unconfigured@null.spigotmc.org>
2025-04-29 23:15:31 +0400

    Initial commit

---
+++ src/main/java/by/mrflaxe/petbox/PetboxApplication.java
@@ -0,0 +1,13 @@
+package by.mrflaxe.petbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/mrflaxe/petbox/PetboxApplicationTests.java
@@ -0,0 +1,13 @@
+package by.mrflaxe.petbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
